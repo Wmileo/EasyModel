b9ffe15cac9858536e2fd9e0fc2be6d837b810fa
ileo <ileo@baozundeMacBook-Pro.local>
2014-10-22 12:13:20 +0800

    Initial Commit

---
+++ EasyModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyModel
+//
+//  Created by ileo on 14/10/22.
+//  Copyright (c) 2014年 ileo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyModel
+//
+//  Created by ileo on 14/10/22.
+//  Copyright (c) 2014年 ileo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
